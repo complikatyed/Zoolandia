a1e911f27774f6d36d58b3078ed81100fa3253b3
complikatyed <katye.russell@gmail.com>
2016-07-22 19:41:20 -0500

    Instantiate new animals and add info about their properties and methods

--- Program.cs
+++ Program.cs
@@ -9,13 +9,34 @@ namespace Zoolandia
         {
             Console.WriteLine("We're making a groovy zoo!");
 
-            Alligator alligator = new Alligator("Jo-Bob");
-            Console.WriteLine("Meet our new alligator, named " + alligator.Name);
-            Console.WriteLine("The alligator weighs " + alligator.Weight + " pounds.");
-
-
-
-
+            Lion lion = new Lion("Jo-Bob");
+            Console.WriteLine("Meet our new lion, named " + lion.IndividualName);
+            Console.WriteLine("Lions are members of the class " + lion.ClassName);
+            Console.WriteLine("Their species name is " + lion.SpeciesName);
+            lion.Sleep();
+            lion.Eat();
+            Console.WriteLine(" ");
+            Giraffe giraffe = new Giraffe("Raffi");
+            Console.WriteLine("Meet our new giraffe, named " + giraffe.IndividualName);
+            Console.WriteLine("Giraffes are members of the class " + giraffe.ClassName);
+            Console.WriteLine("Their species name is " + giraffe.SpeciesName);
+            giraffe.Sleep();
+            giraffe.Eat();
+            Console.WriteLine(" ");
+            Python python = new Python("Slinky");
+            Console.WriteLine("Our new python is named " + python.IndividualName);
+            Console.WriteLine("Pythons are members of the class " + python.ClassName);
+            Console.WriteLine("Their species name is " + python.SpeciesName);
+            python.Sleep();
+            python.Eat();
+            Console.WriteLine(" ");
+            Bonobo bonobo = new Bonobo("Dave");
+            Console.WriteLine("Our new bonobo is named " + bonobo.IndividualName);
+            Console.WriteLine("Bonobos are members of the class " + bonobo.ClassName);
+            Console.WriteLine("Their species name is " + bonobo.SpeciesName);
+            bonobo.Sleep();
+            bonobo.Eat();
+            Console.WriteLine(" ");
 
 
 
