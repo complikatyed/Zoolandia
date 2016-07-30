ecabe40f63738f97d4bd73d059a1feb92a763204
complikatyed <katye.russell@gmail.com>
2016-07-30 14:26:03 -0500

    Change diet property setters and add Introduction method (probably temporarily)

--- Species/Bonobo.cs
+++ Species/Bonobo.cs
@@ -1,14 +1,13 @@
 using System;
-using Zoolandia;
+using Zoolandia.Genus;
 
 namespace Zoolandia.Species
 {
-    public class Bonobo : Animal
+    public class Bonobo : Mammalia
     {
 
-        public Bonobo(string nickname)
+        public Bonobo(string nickname) : base()
         {
-           this.ClassName = "Mammalia";
            this.SpeciesName = "Pan paniscus";
            this.CommonName = "bonobo";
            this.IndividualName = nickname;
@@ -19,6 +18,13 @@ namespace Zoolandia.Species
         public override void Sleep()
         {
             Console.WriteLine("Bonobos typically sleep in trees and are diurnal.");
+        }
+
+        public virtual void Introduction()
+        {
+            Console.WriteLine("Meet our new " + this.CommonName + ", named " + this.IndividualName + ".");
+            Console.WriteLine(this.CommonName + "s are members of the class " + this.GenusName + ".");
+            Console.WriteLine("Their species name is " + this.SpeciesName + ".");
         }
 
 
