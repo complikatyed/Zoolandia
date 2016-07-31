3c9df4e98888c00daceab6d2802e16c17eb04baf
complikatyed <katye.russell@gmail.com>
2016-07-31 14:15:41 -0500

    Remove all calls except instantiation of animals

--- Program.cs
+++ Program.cs
@@ -11,35 +11,13 @@ namespace Zoolandia
             Console.WriteLine("We're making a groovy zoo!" + System.Environment.NewLine);
 
             Lion lion = new Lion("Jo-Bob");
-            lion.Introduction();
-            lion.Sleep();
-            lion.Eat();
-            Console.WriteLine(" ");
-            
             Giraffe giraffe1 = new Giraffe("Raffi");
             Giraffe giraffe2 = new Giraffe("Barb", 3);
-            giraffe1.Introduction();
-            giraffe2.Introduction();
-            giraffe1.Sleep();
-            giraffe2.Sleep();
-            giraffe1.Eat();
-            giraffe2.Eat();
-            Console.WriteLine(" ");
-            
-            Python python = new Python("Slinky");
-            python.Introduction();
-            python.Sleep();
-            python.Eat();
-            Console.WriteLine(" ");
-            
+            Python python = new Python("Slinky");            
             Bonobo bonobo = new Bonobo("Dave");
-            bonobo.Introduction();
-            bonobo.Sleep();
-            bonobo.Eat();
-            Console.WriteLine(" ");
+            Penguin penguin = new Penguin("Pogo");
             
-            Console.WriteLine(bonobo.Die());
-            Console.WriteLine(giraffe1.Die());
+
 
         }
     }
