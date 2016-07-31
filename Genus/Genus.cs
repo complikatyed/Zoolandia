6fec7a19450bfb94a4ed33a55c2ddbdc0afef646
complikatyed <katye.russell@gmail.com>
2016-07-31 09:58:57 -0500

    Include Introduction method and fix UppercaseFirst method in message.

--- Genus/Genus.cs
+++ Genus/Genus.cs
@@ -1,5 +1,18 @@
+using System;
+
 namespace Zoolandia.Genus
 {
     public class Genus : Animal
-    {}
+    {
+        public string SpeciesName { get; set;}
+        public void Introduction()
+        {
+            Console.WriteLine("Meet our new " + this.CommonName + ", named " + this.IndividualName + ".");
+            Console.WriteLine(UppercaseFirst(this.CommonName) + "s are members of the class " + this.GenusName + ".");
+            Console.WriteLine("Their species name is " + this.SpeciesName + ".");
+        }
+
+
+
+    }
 }
